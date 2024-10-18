a265ec27b8ddfee94df6ca9290c023d76e310ee4
firdoseara <143375967+firdoseara@users.noreply.github.com>
2024-10-18 19:33:13 +0530

    Delete Frontend/scripts.js

--- Frontend/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menu = document.querySelector('.menu');
-    const nav = document.querySelector('.nav');
-
-    menu.addEventListener('click', function() {
-        nav.classList.toggle('active');
-    });
-});
